69816f079fd7808c3158236ac1e70ba89c6a0f1e
Agana-Joseph <josephagana8@gmail.com>
2023-08-19 20:41:01 +0100

    Just here batling with React props, come help a brother lol

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Pages from "./pages/Pages"
 import ClassProps from "./ClassProps";
+import FuntionProps from "./FuntionProps";
 function App() {
   return (
     <div className="App">
-      <ClassProps name = "Agana" />
-      <ClassProps name = "Onahi" />
-      <ClassProps name = "Ajom"> <p>Child component</p></ClassProps>
+      <ClassProps name = "Agana" ><p>Child component</p></ClassProps>
+      <ClassProps name = "Onahi" ><button>click</button></ClassProps>
+      <ClassProps name = "Ajom"/>
+      <FuntionProps name = "Leaner1" location = "PHC"/>
       <Pages />
     </div>
   );
---
+++ src/FuntionProps.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function FuntionProps(){
+    return( <div>
+        <h1>This is funtional Component</h1>
+        <h1>Welcome gentle leanr4!</h1>
+    </div>
+    )
+}
+export default FuntionProps;
