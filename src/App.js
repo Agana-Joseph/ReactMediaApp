8cb7c7ba366c2a3be868cd0de38a834a29aff563
Agana-Joseph <josephagana8@gmail.com>
2023-08-19 19:19:25 +0100

    Just here batling with React props, come help a brother lol

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import Pages from "./pages/Pages"
-import FuntionalComp from "./pages/Components/FunctionCom";
-import {ClassComponent, ClassComponent1} from "./pages/Components/ClassComp";
-import Click from "./pages/Components/Click";
-import Counter from "./pages/Components/Counter";
-import ParentComp from "./pages/Components/ParentComp";
 function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
       <Pages />
-      <ClassComponent />
-      <ClassComponent1 />
-      <FuntionalComp />
-      <Click />
-      <Counter />
-      <ParentComp />
     </div>
   );
 }
