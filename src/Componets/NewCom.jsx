78bb23a07e833759ff7d33cbde62fe890cea6eed
Agana-Joseph <josephagana8@gmail.com>
2023-09-26 13:06:24 +0100

    Updating

--- src/Componets/NewCom.jsx
+++ src/Componets/NewCom.jsx
@@ -1,16 +1,43 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import bell from "./bell.png";
+import bellX from "./bellX.png"
 
 export class NewCom extends Component {
   constructor(props) {
     super(props)
   
     this.state = {
-       message: "Send me A Hello"
+       message: "Send me A Hello",
+       sub: "Subscribe",
+       bellUrl: bellX
     };
+  }
+  styles = {
+    fontStyle: "italic",
+    color: "purple"
+  };
+  bellStyles = {
+    width: "50px"
+  };
+
+  buttonChange = () =>{
+    this.setState({
+      message: "Hit the bell to get notification",
+   });
+  }
+  imageChange = () =>{
+    this.setState({
+      bellUrl: bell
+      message: "Than you 4 sub",
+    })
   }
   render() {
     return (
-      <div className=''><h3>{this.state.message}</h3></div>
+      <div className='App'><h3 style={this.styles}>{this.state.message}</h3>
+            <button onClick={this.buttonChange}>{this.state.sub}</button>
+            <p/>
+            <img src={this.state.bellUrl} alt="" style={this.bellStyles} onClick={this.imageChange}/>
+      </div>
     )
   }
 }
