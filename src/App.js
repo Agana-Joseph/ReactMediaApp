def80d7813cd42dcf397a7ffe87888baaf3c5b6e
Agana-Joseph <josephagana8@gmail.com>
2023-10-06 02:52:58 +0100

    message

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import './App.scss';
 import Header from "./Componets/Header";
-
+import Main from "./Componets/Main"
 function App (){
   return(
     <div className="App">
       <Header />
+      <Main />
     </div>
   );
 }
