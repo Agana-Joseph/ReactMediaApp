540e67ba408931abfe74342f2d2f4f75f9da6db7
Agana-Joseph <josephagana8@gmail.com>
2023-10-07 10:54:15 +0100

    Instagram clone

--- src/Components/Login/LoginPage.jsx
+++ src/Components/Login/LoginPage.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Grid from '@material-ui/core/Grid'
 import inst_image from '../images/inst_image.png'
 import inst_name from '../images/inst_name.png'
+import fbIcon from '../images/fbIcon.avif'
 import './LoginPage.css'
 
 export class LoginPage extends Component {
@@ -24,10 +25,26 @@ export class LoginPage extends Component {
                         <img className='loginPage_logo' src={inst_name} alt="" />
                         <div>
                             <div className='loginPage_signin'>
-                                <input className='loginPage_txt' type="text" />
-                                <input className='loginPage_txt' type="password" />
+                                <input className='loginPage_txt' type="text" placeholder='Phone number, username or email'/>
+                                <input className='loginPage_txt' type="password" placeholder='password' />
                                 <button className='login_btn'>Sign In</button>
                             </div>
+
+                            <div className='login-ordiv'>
+                                <div className='login_dividor'></div>
+                                <div className='login_or'>OR</div>
+                                <div className='login_dividor'></div>
+                            </div>
+
+                            <div className='login_fb'>
+                            <img className='loginPage_logo' src={fbIcon} alt="" style={{"marginRight" : "5px"}} /> Login with Facbook
+                            </div>
+                            <div className='login_forgot'>Forgot password</div>
+                        </div>
+                    </div>
+                    <div className='loginPage_signUpOption'>
+                        <div className='loginPage_signUp'>
+                            Don't have an account? Sign Up
                         </div>
                     </div>
                 </div>
